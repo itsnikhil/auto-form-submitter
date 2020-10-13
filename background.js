a883cf3ff50845c2df241dc1b99eb136c6b7b09d
Nikhil Taneja <taneja.nikhil03@gmail.com>
2020-10-13 18:54:22 +0530

    Safety lock added

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 // background.js (c) 2020
 // Desc: Run code in background
 // Created:  Thu Oct 08 2020 17:37:15 GMT+0530 (India Standard Time)
-// Modified: Tue Oct 13 2020 14:31:44 GMT+0530 (India Standard Time)
+// Modified: Tue Oct 13 2020 18:52:16 GMT+0530 (India Standard Time)
 // 
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -20,30 +20,34 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.tabs.query({ url: "*://forms.microsoft.com/Pages/ResponsePage.aspx?id=*" }, (formtabs) => {
-        formtabs.forEach(tab => {
-            chrome.tabs.executeScript(tab.id, {
-                code:
-                    `
-                    if(document.querySelector(".office-form-bottom-button"))
-                        document.querySelector(".office-form-bottom-button").click();
-                    else console.log('Something went wrong!');
-                    `
+    chrome.storage.sync.get(['time', 'enabled'], function (result) {
+        if(result.enabled){
+            chrome.tabs.query({ url: "*://forms.microsoft.com/Pages/ResponsePage.aspx?id=*" }, (formtabs) => {
+                formtabs.forEach(tab => {
+                    chrome.tabs.executeScript(tab.id, {
+                        code:
+                            `
+                            if(document.querySelector(".office-form-bottom-button"))
+                                document.querySelector(".office-form-bottom-button").click();
+                            else console.log('Something went wrong!');
+                            `
+                    });
+                });
             });
-        });
-    });
-    chrome.tabs.query({ url: "*://forms.office.com/Pages/ResponsePage.aspx?id=*" }, (formtabs) => {
-        formtabs.forEach(tab => {
-            chrome.tabs.executeScript(tab.id, {
-                code:
-                    `
-                        if(document.querySelector(".office-form-bottom-button"))
-                            document.querySelector(".office-form-bottom-button").click();
-                        else console.log('Something went wrong!');
-                        `
+            chrome.tabs.query({ url: "*://forms.office.com/Pages/ResponsePage.aspx?id=*" }, (formtabs) => {
+                formtabs.forEach(tab => {
+                    chrome.tabs.executeScript(tab.id, {
+                        code:
+                            `
+                                if(document.querySelector(".office-form-bottom-button"))
+                                    document.querySelector(".office-form-bottom-button").click();
+                                else console.log('Something went wrong!');
+                                `
+                    });
+                });
             });
-        });
+        }
+        chrome.runtime.sendMessage({ success: true });
     });
-    chrome.runtime.sendMessage({ success: true });
 });
 
--- content_script.js
+++ content_script.js
@@ -3,7 +3,8 @@
 // content_script.js (c) 2020
 // Desc: description
 // Created:  Sun Oct 11 2020 12:56:31 GMT+0530 (India Standard Time)
-// Modified: Sun Oct 11 2020 13:02:57 GMT+0530 (India Standard Time)
+// Modified: Tue Oct 13 2020 18:50:20 GMT+0530 (India Standard Time)
 // 
 
+chrome.storage.sync.set({ 'enabled': false }, () => { });
 chrome.runtime.sendMessage({type: 'showPageAction'});
